f6d6e5b1aa51d42851b09a80ed1bf387a7f20b38
Dylan Sharp <dylan@praxispioneering.com>
2022-08-15 13:44:55 -0400

    Try with get_running_loop instead of get_current_loop

--- jupyter_dash/comms.py
+++ jupyter_dash/comms.py
@@ -4,9 +4,6 @@ from ipykernel.comm import Comm
 import nest_asyncio
 import time
 import sys
-import ipykernel
-from nbclient.util import just_run
-import ipython_blocking
 
 _jupyter_config = {}
 
@@ -49,19 +46,55 @@ def _request_jupyter_config(timeout=2):
         # Not in jupyter setting
         return
 
+    _send_jupyter_config_comm_request()
+
+    # Get shell and kernel
+    shell = IPython.get_ipython()
+    kernel = shell.kernel
+
+    # Start capturing shell events to replay later
+    captured_events = []
+
+    def capture_event(stream, ident, parent):
+        captured_events.append((stream, ident, parent))
+
+    kernel.shell_handlers['execute_request'] = capture_event
+
+    # increment execution count to avoid collision error
+    shell.execution_count += 1
+
     # Allow kernel to execute comms until we receive the jupyter configuration comm
     # response
     t0 = time.time()
-    ctx = ipython_blocking.CaptureExecution(replay=True)
-    with ctx:
-        _send_jupyter_config_comm_request()
-        while True:
-            if (time.time() - t0) > timeout:
-                # give up
-                raise EnvironmentError(
-                    "Unable to communicate with the jupyter_dash notebook or JupyterLab \n"
-                    "extension required to infer Jupyter configuration."
-                )
-            if _jupyter_comm_response_received():
-                break
-            ctx.step()
+    while True:
+        if (time.time() - t0) > timeout:
+            # give up
+            raise EnvironmentError(
+                "Unable to communicate with the jupyter_dash notebook or JupyterLab \n"
+                "extension required to infer Jupyter configuration."
+            )
+        if _jupyter_comm_response_received():
+            break
+
+        if asyncio.iscoroutinefunction(kernel.do_one_iteration):
+            loop = asyncio.get_running_loop()
+            nest_asyncio.apply(loop)
+            loop.run_until_complete(kernel.do_one_iteration())
+        else:
+            kernel.do_one_iteration()
+
+    # Stop capturing events, revert the kernel shell handler to the default
+    # execute_request behavior
+    kernel.shell_handlers['execute_request'] = kernel.execute_request
+
+    # Replay captured events
+    # need to flush before replaying so messages show up in current cell not
+    # replay cells
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    for stream, ident, parent in captured_events:
+        # Using kernel.set_parent is the key to getting the output of the replayed
+        # events to show up in the cells that were captured instead of the current cell
+        kernel.set_parent(ident, parent)
+        kernel.execute_request(stream, ident, parent)
